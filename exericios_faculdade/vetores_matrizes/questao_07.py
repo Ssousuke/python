414eed4676dd8a206b02ef59e63c73c94e112258
wesleysousuke <wesleysousukee@gmail.com>
2021-10-14 17:12:37 -0300

    calculando notas

---
+++ exericios_faculdade/vetores_matrizes/questao_07.py
@@ -0,0 +1,31 @@
+class MediaALuno:
+    notas = []
+
+    def add_notas(self, nota):
+        self.notas.append(nota)
+
+    def calcula_notas(self):
+        soma = 0
+        for x in self.notas:
+            soma += x
+        media = (soma / len(self.notas))
+        return media
+
+    # def soma(self):
+    # return sum(self.notas)
+
+
+if __name__ == "__main__":
+    aluno1 = MediaALuno()
+    x = 8
+    while x > 0:
+        try:
+            nota = int(input(f'Digite a {x}º: '))
+            if 0 <= nota <= 10:
+                aluno1.add_notas(nota)
+                x -= 1
+            else:
+                print('A nota deve estar entre 0 e 10!')
+        except Exception as e:
+            print(f'Ocorreu um erro: {e}')
+    print(aluno1.calcula_notas())
